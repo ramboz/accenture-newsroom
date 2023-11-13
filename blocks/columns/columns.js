ad9f5cd8fabf11f72cb0f34df7d635dd88d66c11
Ravi Kiran Papani <ravikiran.papani@gmail.com>
2023-11-13 14:34:40 -0800

    fix: pr contacts image max width and mail to links (#445)

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -15,4 +15,13 @@ export default function decorate(block) {
       }
     });
   });
+
+  // convert mailto links to email
+  block.querySelectorAll('a[href^="mailto"]').forEach((link) => {
+    const img = document.createElement('img');
+    img.src = '/icons/email.png';
+    img.alt = 'email icon';
+    link.innerHTML = '';
+    link.prepend(img);
+  });
 }
