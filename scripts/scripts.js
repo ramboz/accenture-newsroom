f366dd7862fcb29c5737411b2aba4dc87406adab
Ravi Kiran Papani <ravikiran.papani@gmail.com>
2023-11-22 14:05:40 -0800

    fix: removing word boundaries from abstract finding regex (#438)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -32,7 +32,7 @@ import {
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 // regex to find abstract paragraph
-export const ABSTRACT_REGEX = /(.*?);.*?(\d{4})|(.*?)(\d{4})\s+–\s+\b|(.*?)(\d{4})\s+-\s+\b/;
+export const ABSTRACT_REGEX = /(.*?);.*?(\d{4})|(.*?)(\d{4})\s*(–|-|‒|:)\s*/;
 
 export const isMobile = () => window.innerWidth < 600;
 
